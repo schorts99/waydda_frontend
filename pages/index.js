ac934cec3f0873590db58b77a4693c64eac78b70
Angel Mendez <angelmendezz751@gmail.com>
2020-07-12 16:11:58 -0500

    refactor: Dynamic import in index page

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,9 @@ import {head} from '../locales/pages/index.json'
 import Container from "../components/Container";
 import BannerWelcome from "../components/BannerWelcome";
 import SimpleRegister from "../components/SimpleRegister";
-import ResponsiveImage from "../components/ResponsiveImage";
-import HowItWork from "../components/HowItWork";
-import RetailBanner from "../components/RetailBanner";
+import dynamic from 'next/dynamic'
+const RetailBanner = dynamic(() => import('../components/RetailBanner'))
+const HowItWork = dynamic(() => import('../components/HowItWork'))
 
 export default function Home() {
 	return (
