bbca4aa0d361a97080ab257fd896d626da985c3e
Angel Mendez <angelmendezz751@gmail.com>
2020-07-21 00:33:39 -0500

    Fix: Export default page

---
+++ pages/dashboard/categories/index.jsx
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) AzaChii and its affiliates. All Rights Reserved.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ *
+ * Proprietary and confidential
+ *
+ * Written by Angel Mendez <hello@azachii.dev>, Jun 2020
+ *
+ * https://azachii.dev
+ *
+ * LICENSE file in the root directory of this source tree.
+ */
+
+export default function DashboardCategoriesPage() {
+	return (null)
+}
