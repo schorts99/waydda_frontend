43f5fb4f774b29055312318163c72281f0c45a77
Angel Mendez <angelmendezz751@gmail.com>
2020-07-22 15:41:51 -0500

    style: Whatsapp button

--- components/WhatsAppButton/index.jsx
+++ components/WhatsAppButton/index.jsx
@@ -19,10 +19,30 @@ export default function WhatsAppButton({data: {number, message, style}}) {
 	
 	const [rounded, setRounded] = useState(false);
 	
-	
-	if (style) {
-		return (
-			<div className="fixed bottom-0 z-20 right-0 mb-6 mr-6">
+	return (
+		<>
+			<a
+				target={"_blank"}
+				href={`https://wa.me/${number}?text=${message}`}
+				className={`grid grid-cols-1 z-20 py-3 items-center rounded shadow-2xl fixed bottom-0 w-11/12 right-0 mb-5 mx-auto left-0 ${style ? "hidden md:block" : "block md:hidden"}`}
+				style={{
+					background: "#075E54"
+				}}
+			>
+				<div className="col-span-1 text-center relative">
+					<div className="col-span-2 absolute left-0 ml-5 text-center flex justify-center items-center">
+						<FaWhatsapp
+							size={20}
+							color={"white"}
+						/>
+					</div>
+					<p className="text-white font-bold">Contactar por whatsapp</p>
+				</div>
+			</a>
+			<a
+				href={`https://wa.me/${number}?text=${message}`}
+				target="_blank"
+				className={`fixed bottom-0 z-20 right-0 mb-6 mr-6 ${style ? "block" : "hidden md:block"}`}>
 				<button
 					// onClick={handleClick}
 					style={{background: "#075E54"}}
@@ -34,29 +54,7 @@ export default function WhatsAppButton({data: {number, message, style}}) {
 						/>
 					</div>
 				</button>
-			</div>
-		)
-	}
-	
-	return (
-		<a
-			target={"_blank"}
-			href={`https://wa.me/${number}?text=${message}`}
-			// onClick={handleClick}
-			className="grid grid-cols-1 z-20 py-3 items-center rounded shadow-2xl fixed bottom-0 w-11/12 right-0 mb-5 mx-auto left-0"
-			style={{
-				background: "#075E54"
-			}}
-		>
-			<div className="col-span-1 text-center relative">
-				<div className="col-span-2 absolute left-0 ml-5 text-center flex justify-center items-center">
-					<FaWhatsapp
-						size={20}
-						color={"white"}
-					/>
-				</div>
-				<p className="text-white font-bold">Contactar por whatsapp</p>
-			</div>
-		</a>
+			</a>
+		</>
 	)
 }
